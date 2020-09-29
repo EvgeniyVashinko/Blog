57c112cdf0acbde0d97eeb051904ae192fce8434
EvgeniyVashinko <48275225+EvgeniyVashinko@users.noreply.github.com>
2020-09-29 14:08:04 +0300

    fix login view and add homepage

--- Blog/Controllers/HomeController.cs
+++ Blog/Controllers/HomeController.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Blog.Domain;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly DataManager dataManager;
+        public HomeController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
         public IActionResult Index()
         {
-            return View();
+            return View(dataManager.Articles.GetArticles());
         }
     }
 }
