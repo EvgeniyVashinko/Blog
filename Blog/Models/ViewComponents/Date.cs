f29faace5cebcdd5bc1eaa7b879075ee38bd37de
EvgeniyVashinko <48275225+EvgeniyVashinko@users.noreply.github.com>
2020-10-02 00:05:59 +0300

    Add viewComponents

---
+++ Blog/Models/ViewComponents/Date.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Models.ViewComponents
+{
+    public class Date : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            DateTime date = DateTime.Now;
+            return new HtmlContentViewComponentResult(
+                new HtmlString($"<ul>{date.ToString("dd.MM.yyyy")}, {date.DayOfWeek}</ul>"));
+        }
+    }
+}
---
+++ Blog/Models/ViewComponents/Info.cs
@@ -0,0 +1,45 @@
+﻿using Blog.Domain;
+using Blog.Domain.Entities;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Models.ViewComponents
+{
+    public class Info : ViewComponent
+    {
+        private readonly DataManager dataManager;
+        private readonly UserManager<User> userManager;
+        public Info(DataManager dataManager, UserManager<User> userManager)
+        {
+            this.dataManager = dataManager;
+            this.userManager = userManager;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var articlesCount = dataManager.Articles.GetArticles().Count();
+            var commentsCount = dataManager.Comments.GetComments().Count();
+            var usersCount = userManager.Users.Count();
+            Information info = new Information(articlesCount, commentsCount, usersCount);
+            return View("Info", info);
+        }
+    }
+    public class Information
+    {
+        public int articlesCount { get; set; }
+        public int commentsCount { get; set; }
+        public int usersCount { get; set; }
+        public Information(int articlesCount, int commentsCount, int usersCount)
+        {
+            this.articlesCount = articlesCount;
+            this.commentsCount = commentsCount;
+            this.usersCount = usersCount;
+        }
+
+    }
+}
