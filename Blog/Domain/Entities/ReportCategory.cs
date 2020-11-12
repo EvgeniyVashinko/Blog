164b754aac91b960c684a7f0a0cf33d8c67b11db
EvgeniyVashinko <48275225+EvgeniyVashinko@users.noreply.github.com>
2020-11-12 15:19:00 +0300

    fix reports localization

--- Blog/Domain/Entities/ReportCategory.cs
+++ Blog/Domain/Entities/ReportCategory.cs
@@ -14,5 +14,20 @@ namespace Blog.Domain.Entities
         public string BeName { get; set; }
         public string EnName { get; set; }
         public List<Report> Reports { get; set; }
+
+        public string GetLocalizedName(string langcode)
+        {
+            switch (langcode)
+            {
+                case "ru":
+                    return Name;
+                case "be":
+                    return BeName;
+                case "en":
+                    return EnName;
+                default:
+                    return default;   
+            }
+        }
     }
 }
